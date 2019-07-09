b3a5dcf6be8634015110fe0ee15114c337290def
Bob Brown <rb3@tutanota.com>
2019-07-09 08:45:08 -0400

    added working_dir variable to login_ftp and removed hard coded string for ftp.cwd()

--- rwsa.py
+++ rwsa.py
@@ -1,10 +1,10 @@
 from ftplib import FTP
 
 
-def login_ftp(url, username, password):
+def login_ftp(url, username, password, working_dir):
 	ftp = FTP(url)
 	ftp.login(user=username, passwd=password)
 	#ftp.dir()
-	ftp.cwd('GIS Files/RWSA')
+	ftp.cwd(working_dir)
 	ftp.dir()
 	ftp.quit()
