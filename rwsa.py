30305b64c485a58ed5fc07ee7763a32ae1731fae
Bob Brown <rb3@tutanota.com>
2019-07-09 10:48:36 -0400

    added logic to pull most recent file. entirely depended on file name, not Date Modified. May go back and try to get .mlsd() working so I can use Date Modified instead

--- rwsa.py
+++ rwsa.py
@@ -1,12 +1,28 @@
 from ftplib import FTP
+import datetime
 
 def login_ftp(url, username, password, working_dir):
 	ftp = FTP(url)
 	ftp.login(user=username, passwd=password)
 	ftp.cwd(working_dir)
-	ftp.dir()
-	#filename = "RWSA_2019_06_10.gdb.zip"
-	#localfile = open(filename, 'wb')
+	#ftp.dir()
+	most_recent_date = datetime.date(2000,1,1)
+	most_recent_file = str()
+	for x in ftp.nlst():
+		split_list = x.split('_')
+		try:
+			year = int(split_list[1])
+			month = int(split_list[2])
+			day = int(split_list[3].split('.')[0])
+			d = datetime.date(year,month,day)
+			if d > most_recent_date:
+				most_recent_date = d
+				most_recent_file = x
+		except:
+			pass
+	print(most_recent_date, most_recent_file)
+	filename = most_recent_file
+	localfile = open(filename, 'wb')
 
-	#ftp.retrbinary("RETR " + filename, localfile.write, 1024)
+	ftp.retrbinary("RETR " + filename, localfile.write, 1024)
 	ftp.quit()
