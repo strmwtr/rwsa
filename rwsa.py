db337cfd8e719111a23471961775fb7d53bb159b
Bob Brown <rb3@tutanota.com>
2019-07-09 09:34:29 -0400

    got download to work with hardcoded filename.

--- rwsa.py
+++ rwsa.py
@@ -1,10 +1,12 @@
 from ftplib import FTP
 
-
 def login_ftp(url, username, password, working_dir):
 	ftp = FTP(url)
 	ftp.login(user=username, passwd=password)
-	#ftp.dir()
 	ftp.cwd(working_dir)
 	ftp.dir()
+	#filename = "RWSA_2019_06_10.gdb.zip"
+	#localfile = open(filename, 'wb')
+
+	#ftp.retrbinary("RETR " + filename, localfile.write, 1024)
 	ftp.quit()
