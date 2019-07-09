1c274e3db35dbb95eb4bddfca2ef1e1ad8a20e4f
Bob Brown <rb3@tutanota.com>
2019-07-09 08:41:03 -0400

    ftp_login working, Error was cause by passing ftp:// before ftp. Removed and now it works

--- rwsa.py
+++ rwsa.py
@@ -4,4 +4,7 @@ from ftplib import FTP
 def login_ftp(url, username, password):
 	ftp = FTP(url)
 	ftp.login(user=username, passwd=password)
+	#ftp.dir()
+	ftp.cwd('GIS Files/RWSA')
+	ftp.dir()
 	ftp.quit()
