741b6193726eabd9bc338cca745b47ffc29c8d82
Bob Brown <rb3@tutanota.com>
2019-07-10 12:44:32 -0400

    Added if logic to get both Sanitary and Water gdb

--- rwsa.py
+++ rwsa.py
@@ -35,31 +35,34 @@ def grab_rwsa_data(url, username, password, working_dir, output_dir):
 def grab_acsa_data(url, username, password, working_dir, output_dir):
 	ftp = FTP(url)
 	ftp.login(user=username, passwd=password)
-	ftp.cwd(working_dir)
-	#ftp.dir()
-	most_recent_date = datetime.date(2000,1,1)
-	most_recent_file = str()
-	for x in ftp.nlst():
-		split_list = x.split('_')
-		try:
-			year = int(split_list[2][0:4])
-			month = int(split_list[2][4:6])
-			day = int(split_list[2][6:8])
-			d = datetime.date(year,month,day)
-			if d > most_recent_date:
-				most_recent_date = d
-				most_recent_file = x
-		except:
-			pass
-	gdb = path.join(output_dir,most_recent_file)
-	mkdir(gdb)
-	ftp.cwd(most_recent_file)
-	for x in ftp.nlst():
-		filename = x
-		out_file = path.join(gdb, x)
-		localfile = open(out_file, 'wb')
-		print(filename, '\n', out_file, '\n','-'*60)
-		ftp.retrbinary("RETR " + filename, localfile.write, 1024)
+	for utility in ['Sanitary', 'Water']:
+		ftp.cwd('/' + working_dir)
+		most_recent_date = datetime.date(2000,1,1)
+		most_recent_file = str()
+		for x in ftp.nlst():
+			split_list = x.split('_')
+			try:
+				if split_list[1] == utility and split_list[2].endswith('.gdb'):
+					year = int(split_list[2][0:4])
+					month = int(split_list[2][4:6])
+					day = int(split_list[2][6:8])
+					d = datetime.date(year,month,day)
+					if d > most_recent_date:
+						most_recent_date = d
+						most_recent_file = x
+			except:
+				pass
+		gdb = path.join(output_dir,most_recent_file)
+		print(gdb)
+		mkdir(gdb)
+		print(ftp.pwd())
+		ftp.cwd(most_recent_file)
+		print(ftp.pwd())
+		for x in ftp.nlst():
+			filename = x
+			out_file = path.join(gdb, x)
+			localfile = open(out_file, 'wb')
+			ftp.retrbinary("RETR " + filename, localfile.write, 1024)
 	ftp.quit()
 
-	return out_file
+	#return out_file
